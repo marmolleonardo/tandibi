7f8bb1c4796e4d38612ab9160251bf4dabc08baa
Leonardo Marmol <marmolleonardo@gmail.com>
2024-03-25 20:38:26 -0300

    Added the user on devise; Added the layouts to the sessions on app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_20_174907) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_25_223028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -75,7 +75,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_174907) do
     t.boolean "is_public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
